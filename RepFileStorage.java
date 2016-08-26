db0e42dfb45cff2aa9e680be6c5fd9b316e5270e
Deborah Lewis <deborahlewis@users.noreply.github.com>
2016-08-26 06:56:38 -0400

    Cleanup of unused code
    Removed DecimalFormat and import methods for number formatting

--- RepFileStorage.java
+++ RepFileStorage.java
@@ -8,8 +8,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -23,22 +21,9 @@ public class RepFileStorage {
             //format doubles to show 2 decimal places like dollar amounts
             try (BufferedWriter fileOut = new BufferedWriter (new FileWriter(textWriter, true)) //open file
             ) {
-                //format doubles to show 2 decimal places like dollar amounts
-                NumberFormat dollar = new DecimalFormat("#0.00");
-                //write to file
-                fileOut.write("" + s); //print SalesRep object (pulled from the toString method
-        
-        /*("" + s.getRepId() //rep id
-                        + " " + s.getFirstName() //space firstName
-                        + " " + s.getLastName() //space lastName
-                        + " SUPPLIES " + dollar.format(s.getSupplies()) //space supplies in dollar format
-                        + " BOOKS " + dollar.format(s.getBooks()) //space books in dollar format
-                        + " PAPER " + dollar.format(s.getPaper()) //space paper in dollar format
-                        + " " + s.getDistrict().toUpperCase() //space district in upper case
-                        + " " + s.getContact());
-                fileOut.newLine(); //next entry begins on new line
                 
-                */
+                //write to file
+                fileOut.write("" + s); //print SalesRep object (pulled from the toString method)
                 fileOut.close();//save & close file
             }            
             //give user feedback
